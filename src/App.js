bd1b10b531516daf952cda037c7fcb71637e9d85
mk1018 <ktmn1018@yahoo.co.jp>
2021-02-12 19:57:12 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
+
+/* jsx （直感的にHTMLを操作できる） */
 function App() {
   return (
-    <div><h1>HelloWorld</h1></div>
+    <h1>HelloWorld</h1> //jsx (javaScriptXMLの略)
   );
 }
+
+/* JavaScript */
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hello World!!"
+//   );
+// }
+
 
 export default App;
