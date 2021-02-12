4018ab029a9865e6b627899ff0e421f891aa62ec
mk1018 <ktmn1018@yahoo.co.jp>
2021-02-12 20:10:53 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 /* jsx （直感的にHTMLを操作できる） */
 function App() {
+  // return <h1 className="foo">HelloWorld</h1> ; //jsx (javaScriptXMLの略)
   return (
-    <h1>HelloWorld</h1> //jsx (javaScriptXMLの略)
+    <>
+      <input type="text" onChange={() => {console.log("i ma clicked.")}} />
+    </>
   );
 }
 
