75a59edead599eff1166a98bb15e37ebfa13953c
mk1018 <ktmn1018@yahoo.co.jp>
2021-02-12 22:09:53 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-/* jsx （直感的にHTMLを操作できる） */
-function App() {
-  // return <h1 className="foo">HelloWorld</h1> ; //jsx (javaScriptXMLの略)
+/**
+ * クラスコンポーネント
+ */
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <input type="text" onChange={() => {console.log("i ma clicked.")}} />
+//       </>
+//     );
+//   }
+// }
+
+/**
+ * 関数コンポーネント１
+ */
+// function App() {
+//   // return <h1 className="foo">HelloWorld</h1> ; //jsx (javaScriptXMLの略)
+//   return (
+//     <>
+//       <input type="text" onChange={() => {console.log("i ma clicked.")}} />
+//     </>
+//   );
+// }
+
+/**
+ * 関数コンポーネント２
+ */
+const App = () => {
   return (
     <>
-      <input type="text" onChange={() => {console.log("i ma clicked.")}} />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </>
   );
 }
 
+const Cat = () => {
+  return (
+    <div>猫</div>
+  );
+}
+
+/**
+ * JSXをJavaScriptで表現したもの
+ */
 /* JavaScript */
 // function App() {
 //   return React.createElement(
