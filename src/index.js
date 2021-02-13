58695a0c6567f17ba7519f5ad9aed6013d9ec0a7
mk1018 <ktmn1018@yahoo.co.jp>
2021-02-13 10:56:37 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+// import App from './App';
+import App from './components/App'
 import reportWebVitals from './reportWebVitals';
 
+// ここで作成されたstoreがすべて集約されている
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+  </Provider>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
