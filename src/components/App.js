63cbc0b9922b3aa7d5ac714896ee310a6e866d6c
mk1018 <ktmn1018@yahoo.co.jp>
2021-02-13 17:29:40 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,63 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions';
 // import propTypes from 'prop-types';
+
+class App extends Component {
+  render () {
+    const props = this.props
+    return (
+      <>
+        <div>value: { props.value }</div>
+        <button onClick={ props.increment }>+1</button>
+        <button onClick={ props.decrement }>-1</button>
+      </>
+    )
+  }
+}
+
+const mapStateToProps = state => ({value: state.count.value})
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+const mapDispatchToProps = ({increment, decrement})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// class Counter extends Component {
+//   constructor (props) {
+//     super(props)
+//     console.log(this.state)
+//     this.state = {count: 0}
+//   }
+
+//   handlePlusButton = () => {
+//     /**
+//      * これはやってはいけない
+//      * this.state = {count: this.state.count + 1}
+//      *
+//      * setStateを使うと必ずrenderが実行されるので再描画まで自動的に行われる
+//      */
+
+//     this.setState({count: this.state.count + 1})
+//   }
+
+//   handleMinusButton = () => {
+//     this.setState({count: this.state.count - 1})
+//   }
+
+//   render () {
+//     console.log('render')
+//     return (
+//       <>
+//         <div>{this.state.count}</div>
+//         <button onClick={this.handlePlusButton}>+1</button>
+//         <button onClick={this.handleMinusButton}>-1</button>
+//       </>
+//     )
+//   }
+// }
 
 /**
  * クラスコンポーネント
@@ -49,7 +107,7 @@ import React, {Component} from 'react';
 /**
  * 関数コンポーネント３
  */
-const App = () => (<Counter></Counter>)
+// const App = () => (<Counter></Counter>)
 
 /**
  * User関数コンポーネント
@@ -62,39 +120,6 @@ const App = () => (<Counter></Counter>)
 // }
 
 
-class Counter extends Component {
-  constructor (props) {
-    super(props)
-    console.log(this.state)
-    this.state = {count: 0}
-  }
-
-  handlePlusButton = () => {
-    /**
-     * これはやってはいけない
-     * this.state = {count: this.state.count + 1}
-     *
-     * setStateを使うと必ずrenderが実行されるので再描画まで自動的に行われる
-     */
-
-    this.setState({count: this.state.count + 1})
-  }
-
-  handleMinusButton = () => {
-    this.setState({count: this.state.count - 1})
-  }
-
-  render () {
-    console.log('render')
-    return (
-      <>
-        <div>{this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
-      </>
-    )
-  }
-}
 
 /**
  * プロップTypes
@@ -126,4 +151,4 @@ class Counter extends Component {
 // }
 
 
-export default App;
+// export default App;
